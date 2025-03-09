7a6b0144a458d338e0d97c6365553ac5b05e6596
Miguel Neiva <miguel.neiva@checkmarx.com>
2025-03-09 23:13:06 +0000

    será

--- entrypoint.py
+++ entrypoint.py
@@ -21,7 +21,6 @@ def clone_repos(repos):
             print(f"Repository {repo_name} already cloned, pulling latest changes...")
             subprocess.run(["git", "-C", target_dir, "pull"], check=True)
         else:
-            print(f"Cloning {repo} into {target_dir}")
             subprocess.run(["git", "clone", "--depth=1", repo, target_dir], check=True)
 
         
@@ -37,8 +36,6 @@ def run_2ms_scan():
     for repo_name in os.listdir(repos_path):
         repo_path = os.path.join(REPOS_DIR, repo_name) 
         sarif_path = os.path.join(RESULTS_DIR, f"{repo_name}.sarif")  
-
-        print(f"Running scan for {repo_name}...")
 
 
         subprocess.run([
