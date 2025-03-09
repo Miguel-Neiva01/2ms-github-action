2f92135afc2023430f146ae8deb7bd98894b665b
Miguel Neiva <miguel.neiva@checkmarx.com>
2025-03-09 19:10:11 +0000

    th

--- entrypoint.py
+++ entrypoint.py
@@ -16,12 +16,12 @@ def clone_repos(repos):
     for repo in repos:
         repo_name = os.path.basename(repo).replace(".git", "")
         target_dir = os.path.join(REPOS_DIR, repo_name)
+        print(f"Cloning {repo} into {target_dir}")
 
         if os.path.exists(target_dir):
             print(f"Repository {repo_name} already cloned, skipping...")
             continue
 
-        print(f"Cloning {repo} into {target_dir}")
         subprocess.run(["git", "clone", "--depth=1", repo, target_dir], check=True)
 
 def run_2ms_scan():
@@ -30,8 +30,8 @@ def run_2ms_scan():
     subprocess.run(["chmod", "-R", "777", RESULTS_DIR], check=True)
 
     workspace = os.getcwd()  
-    repos_path = os.path.join(workspace, REPOS_DIR)
-    results_path = os.path.join(workspace, RESULTS_DIR)
+    repos_path = REPOS_DIR
+    results_path = RESULTS_DIR
 
     for repo_name in os.listdir(repos_path):
         repo_path = os.path.join(REPOS_DIR, repo_name) 
