1ba002699531fdb9b2288dfd17ba400e06d9ac80
Miguel Neiva <miguel.neiva@checkmarx.com>
2025-03-10 16:32:15 +0000

    vver

--- src/main.js
+++ src/main.js
@@ -1,32 +1,30 @@
-require('dotenv').config();  
-
 const fs = require("fs");
 const core = require("@actions/core");
 const github = require("@actions/github");
 const path = require("path");
-const { createComment } = require("./report"); 
+const { createComment } = require("./report");
 
-const repo = {
-  owner: process.env.GITHUB_REPOSITORY.split('/')[0], 
-  repo: process.env.GITHUB_REPOSITORY.split('/')[1],   
-};
-
-const commitSha = process.env.GITHUB_SHA || github.context.sha;  
-const token = process.env.GITHUB_TOKEN; 
-
-console.log("GITHUB_TOKEN:", token);  
+const commitSha = github.context.sha;  // Obtém diretamente do contexto do GitHub
+const token = process.env.GITHUB_TOKEN;  // A variável GITHUB_TOKEN é automaticamente fornecida pelo GitHub
 
 async function postCommitComment(results, commitSha, repo) {
   try {
-    const message = createComment(results);
+    const message = createComment(results);  // Cria a mensagem para o comentário e o job summary
 
     const octokit = github.getOctokit(token);  
 
+    
     await octokit.rest.repos.createCommitComment({
       ...repo,
       commit_sha: commitSha,
       body: message,
     });
+
+ 
+    await core.summary
+      .addRaw(message)  
+      .write(); 
+
   } catch (error) {
     core.setFailed(`Error posting commit comment: ${error.message}`);
   }
@@ -43,10 +41,7 @@ async function run() {
 
     const results = JSON.parse(fs.readFileSync(resultsPath, "utf8"));
 
-    // O commitSha já está sendo carregado acima via process.env.GITHUB_SHA
-    // Não é necessário redefinir aqui
-
-    // Chama a função para postar o comentário no commit
+    const repo = github.context.repo; 
     await postCommitComment(results, commitSha, repo);
 
   } catch (error) {
