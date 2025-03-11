60c4ca30da30c7897dd38f0ba9eca3487199e25a
Miguel Neiva <miguel.neiva@checkmarx.com>
2025-03-11 12:18:51 +0000

    new image

--- src/report.js
+++ src/report.js
@@ -2,15 +2,17 @@ const moment = require('moment');
 const { summary } = require('@actions/core/lib/summary');
 
 function createComment(results) {
-    let message = `\n**2ms version: ${results['version']}**\n`;  
 
-    // Criação da tabela em Markdown
+    let message = `![2ms logo](https://github.com/Miguel-Neiva01/2ms-github-action/blob/main/images/2ms_icon.svg)\n`;
+    message += `\n**2ms version: ${results['version']}**\n`;  
+
+ 
     message += "\n| Repo | Total Items Scanned | Test Passed |\n";
-    message += "|------|---------------------|-------------|\n";  // Linha de separação
+    message += "|------|---------------------|-------------|\n";  
 
-    // Adiciona cada repositório e o status do scan
+
     for (const [repo, data] of Object.entries(results)) {
-        const testPassed = data.repo_scan ? "✅" : "❌";  // Usa um símbolo para mostrar se passou ou falhou
+        const testPassed = data.repo_scan ? "✅" : "❌";  
         message += `| ${repo} | ${data['total-items-scanned']} | ${testPassed} |\n`;
     }
 
